2b74e256e008021d8a8efb2135bbda6347e1b9cf
Miroslav <mircheuss@yandex.ru>
2026-02-03 13:22:07 +0300

    set up basic structure, write ProjectInstaller

---
+++ Assets/Game/Core/Input/KeyboardMouseInputService.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class KeyboardMouseInputService : IInputService
+{
+}
---
+++ Assets/Game/Project/Installers/ProjectInstaller.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Zenject;
+
+public class ProjectInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        SignalBusInstaller.Install(Container);
+
+        Container.DeclareSignal<DamageSignal>();
+        Container.DeclareSignal<EnemyDiedSignal>();
+        Container.DeclareSignal<PlayerDiedSignal>();
+
+        Container
+            .Bind<IInputService>()
+            .To<KeyboardMouseInputService>()
+            .AsSingle();
+        
+        Container
+            .Bind<IEnergyService>()
+            .To<EnergyService>()
+            .AsSingle();
+    }
+}
